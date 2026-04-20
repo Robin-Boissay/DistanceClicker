466b02456b334687ddef5787abc480707a53844f
Robin-Boissay <robinb2004@gmail.com>
2026-04-20 23:11:04 +0200

    add prestige upgrade

--- Assets/Scripts/Model/Milestone/BaseMilestone.cs
+++ Assets/Scripts/Model/Milestone/BaseMilestone.cs
@@ -4,7 +4,8 @@ using BreakInfinity;
 /// <summary>
 /// CLASSE PARENT ABSTRAITE pour toutes les définitions de palier pour les upgrades.
 /// </summary>
-public abstract class BaseMilestone : ScriptableObject
+[System.Serializable]
+public  class BaseMilestone
 {
     public int milestoneLevel;
 
